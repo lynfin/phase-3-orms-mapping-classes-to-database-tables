654405d3b24a0e25ef0a58c22a4e61d8e0d15812
Lyn <lynfinman@gmail.com>
2022-07-04 14:53:26 -0400

    commit tree 610c03d65e071dea45911d089b28d20227f8e1dd

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,76 @@
 class Song
+  attr_accessor :name, :album, :id
 
-  attr_accessor :name, :album
-
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE IF EXISTS songs#{' '}
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, name, album)
+
+    # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM songs')[0][0]
+
+    # return the Ruby instance
+    self
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
+
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    new(id: row[0], name: row[1], album: row[2])
+  end
 end
